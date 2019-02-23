5622e4bb07590da8decfa636a1a16c5e6ddaa6d7
Otto <c.otto@krombacher.de>
2019-02-23 10:13:06 +0100

    added Angular Material

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
